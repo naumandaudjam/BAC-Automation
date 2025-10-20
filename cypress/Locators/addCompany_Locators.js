7def6018f93f34c4971fceeb6005109b0435667a
Nauman Daud Jam <naumandaudjam1996@gmail.com>
2025-10-21 00:38:29 +0500

    Code refactoring

--- cypress/Locators/addCompany_Locators.js
+++ cypress/Locators/addCompany_Locators.js
@@ -1,6 +1,5 @@
 const addCompany_Locators = {
     addCompanyFormUrl_Locator: 'https://bac-test.xpresspago.com//Client/Index',
-    goToCompanyAddForm_Locator: 'https://bac-test.xpresspago.com/Client/Edit/0',
     addCompanyButton_Locator: '#addlink',
     addCompanyName_Locator: '.col-sm-8 > [name="CompanyName"]',
     addRUC_Locator: '[name="CompanyIdentityRUC"]',
