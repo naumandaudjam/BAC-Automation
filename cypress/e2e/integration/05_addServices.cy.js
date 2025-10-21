0c03a4c3277b5bad08a9b2afaef7d502c6361248
Nauman Daud Jam <naumandaudjam1996@gmail.com>
2025-10-22 01:57:38 +0500

    add Services test file implementation

--- cypress/e2e/integration/05_addServices.cy.js
+++ cypress/e2e/integration/05_addServices.cy.js
@@ -3,14 +3,13 @@ import addServices from "../../PageObjects/addServicesPage"
 
 const change_Client = changeClient;
 const add_Services = addServices;
-
 describe("Add Services", () => {
   it("Switch to target client before adding services", () => {
     change_Client.visitDashboard();
     change_Client.tapOnChangeClient();
     change_Client.searchCompany();
     change_Client.selectCompany();
-    add_Services.menuServices();
+    addServices.menuServices();
     
   });
 
